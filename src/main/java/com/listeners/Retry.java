cb7051bf636cc87db5dc2a49c9eeb91b40c006d7
Himanshu <Himansh7786@gmail.com>
2025-08-14 17:54:25 +0530

    Added parameters

--- src/main/java/com/listeners/Retry.java
+++ src/main/java/com/listeners/Retry.java
@@ -4,12 +4,13 @@ import org.testng.IRetryAnalyzer;
 import org.testng.ITestResult;
 
 public class Retry implements IRetryAnalyzer {
- int retry_limit=1;
+ int retry_limit;
  int retry_count=0;
  
 	@Override
 	public boolean retry(ITestResult result) {
 		// TODO Auto-generated method stub
+		retry_limit=Integer.parseInt(System.getProperty("retry-count"));
 		if (retry_count < retry_limit) {
 			retry_count++;
 		      return true;
